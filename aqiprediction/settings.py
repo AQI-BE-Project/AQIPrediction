ba879dc353c7d17d4673346caa6fe13027ab79ba
Aishwarya-Joshi11 <aishwarya.joshi111298@gmail.com>
2024-12-24 23:31:03 -0800

    Migrated database from SQLlite3 to PostgreSQL

--- aqiprediction/settings.py
+++ aqiprediction/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'aqiprediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aqi_db',
+        'USER': 'aqi_user',
+        'PASSWORD': 'stpD@0nq6l#mI%a',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
