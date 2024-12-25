6fce176bbbd0b0eb830430e041e914e2d6a40bfe
Aishwarya-Joshi11 <aishwarya.joshi111298@gmail.com>
2024-12-24 16:02:39 -0800

    chnages

--- aqiprediction/aqiprediction/wsgi.py
+++ aqiprediction/aqiprediction/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aqiprediction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aqiprediction.aqiprediction.settings')
 
 application = get_wsgi_application()
