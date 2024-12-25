c5391608dbdee9105ff31016e624e1d2dfdf8734
Aishwarya-Joshi11 <aishwarya.joshi111298@gmail.com>
2024-12-24 17:42:27 -0800

    updates

--- aqiapp/urls.py
+++ aqiapp/urls.py
@@ -6,6 +6,6 @@ app_name = "aqiapp"
 urlpatterns = [
 
     path('', views.predict, name='prediction_page'),
-    path('aqiapp/', views.predict_chances, name='submit_prediction'),
-    path('aqiapp/', views.view_results, name='results'),
+    path('predict/', views.predict_chances, name='submit_prediction'),
+    path('results/', views.view_results, name='results'),
 ]
