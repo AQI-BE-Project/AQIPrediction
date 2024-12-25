e859c3c3e798b828bf590dca75a8c951ff3499a6
Aishwarya-Joshi11 <aishwarya.joshi111298@gmail.com>
2024-12-24 17:10:56 -0800

    Updated location of the database

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for aqiprediction project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+print("Current Working Directory: ", os.getcwd())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aqiprediction.aqiprediction.settings')
+
+application = get_wsgi_application()
